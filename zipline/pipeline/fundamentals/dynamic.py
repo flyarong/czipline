9fb4dad431a7174ead0233ed8f952a79ee03b0db
ldf <liu.dengfeng@hotmail.com>
2018-07-02 03:58:52 +0800

    更新股票简称写入方法

--- zipline/pipeline/fundamentals/dynamic.py
+++ zipline/pipeline/fundamentals/dynamic.py
@@ -14,12 +14,13 @@ from odo import odo
 
 from cswd.common.utils import data_root, ensure_list
 from cswd.sql.constants import MARGIN_MAPS
-
+from cswd.sql.base import session_scope
+from cswd.sql.models import StockDaily
 from .base import STOCK_DB, bcolz_table_path
 from .utils import _normalize_ad_ts_sid
 
 LABEL_MAPS = {'日期': 'asof_date', '股票代码': 'sid'}
-DYNAMIC_TABLES = ['adjustments', 'margins', 'special_treatments','short_names']
+DYNAMIC_TABLES = ['adjustments', 'margins']
 
 logger = Logger('动态数据')
 
@@ -30,7 +31,8 @@ def normalized_dividend_data(df):
     raw_df.rename(columns=LABEL_MAPS, inplace=True)
     raw_df.rename(columns={'派息': 'amount'}, inplace=True)
     # 截止日期为发放日期前一天
-    raw_df['asof_date'] = pd.to_datetime(raw_df['asof_date']) - pd.Timedelta(days=1)
+    raw_df['asof_date'] = pd.to_datetime(
+        raw_df['asof_date']) - pd.Timedelta(days=1)
     return raw_df
 
 
@@ -41,7 +43,7 @@ def normalized_margins_data(df):
     df.rename(columns={v: k for k, v in MARGIN_MAPS.items()}, inplace=True)
     return df
 
-# # TODO:使用日线数据简化代替
+
 def normalized_short_names_data(df):
     """股票简称数据"""
     # 股票未上市或者新上市，暂无股票代码
@@ -53,26 +55,47 @@ def normalized_short_names_data(df):
     df['asof_date'] = df['asof_date'] - pd.Timedelta(days=1)
     return df
 
+
+def _save_short_name():
+    """存储股票简称变动历史"""
+    with session_scope() as sess:
+        query = sess.query(StockDaily.code, StockDaily.date,
+                           StockDaily.A001_名称).group_by(
+                               StockDaily.code, StockDaily.A001_名称)
+        df = pd.DataFrame.from_records(query.all())
+        df.columns = ['sid', 'asof_date', 'short_name']
+        df['asof_date'] = pd.to_datetime(df['asof_date'], utc=True)
+        df['timestamp'] = df['asof_date']  # - pd.Timedelta(days=1)
+        df['sid'] = df['sid'].map(lambda x: int(x))
+    table_name = 'short_names'
+    # 转换为bcolz格式并存储
+    rootdir = bcolz_table_path(table_name)
+    if os.path.exists(rootdir):
+        rmtree(rootdir)
+    odo(df, rootdir)
+    logger.info('表:{}，数据存储路径：{}'.format(table_name, rootdir))
+
+
 # #　TODO:股票简称-> 转换
-def normalized_special_treatments_data(df):
-    """特殊处理历史"""
-    # 股票未上市或者新上市，暂无股票代码
-    df = df[~df['股票代码'].isna()]
-    df = df[['股票代码', '日期', '特别处理']].copy()
-    df.rename(columns=LABEL_MAPS, inplace=True)
-    df.rename(columns={'特别处理': 'treatment'}, inplace=True)
-    # 原为实施日期，截止日期前移一天
-    df['asof_date'] = df['asof_date'] - pd.Timedelta(days=1) 
-    return df
+# def normalized_special_treatments_data(df):
+#     """特殊处理历史"""
+#     # 股票未上市或者新上市，暂无股票代码
+#     df = df[~df['股票代码'].isna()]
+#     df = df[['股票代码', '日期', '特别处理']].copy()
+#     df.rename(columns=LABEL_MAPS, inplace=True)
+#     df.rename(columns={'特别处理': 'treatment'}, inplace=True)
+#     # 原为实施日期，截止日期前移一天
+#     df['asof_date'] = df['asof_date'] - pd.Timedelta(days=1)
+#     return df
 
 
 def _factory(table_name):
     if table_name == 'adjustments':
         return normalized_dividend_data
-    elif table_name == 'short_names':
-        return normalized_short_names_data
-    elif table_name == 'special_treatments':
-        return normalized_special_treatments_data
+    # elif table_name == 'short_names':
+    #     return normalized_short_names_data
+    # elif table_name == 'special_treatments':
+    #     return normalized_special_treatments_data
     elif table_name == 'margins':
         return normalized_margins_data
     raise NotImplementedError(table_name)
@@ -114,7 +137,7 @@ def write_dynamic_data_to_bcolz(tables=None):
     if not tables:
         to_does = DYNAMIC_TABLES
     else:
-        to_does = ensure_list(to_does)
+        to_does = ensure_list(tables)
     for table in to_does:
         if table in DYNAMIC_TABLES:
             ndays = 0
@@ -122,5 +145,6 @@ def write_dynamic_data_to_bcolz(tables=None):
             expr = STOCK_DB[table]
             _write_by_expr(expr, ndays)
         else:
-            raise ValueError('要写入的数据表{}不在"{}"范围'.format(
-                table, DYNAMIC_TABLES))
+            raise ValueError('要写入的数据表{}不在"{}"范围'.format(table, DYNAMIC_TABLES))
+    # # 单独处理股票简称
+    _save_short_name()
